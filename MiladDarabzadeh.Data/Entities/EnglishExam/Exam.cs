5f08a16647bb9a43f65a60e3caff44b6b2f3eda5
ali barzegar <alibarzegar013@gmail.com>
2024-07-27 07:37:58 -0700

    #Structuring Database

--- MiladDarabzadeh.Data/Entities/EnglishExam/Exam.cs
+++ MiladDarabzadeh.Data/Entities/EnglishExam/Exam.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using MiladDarabzadeh.Data.Entities.Course;
+using MiladDarabzadeh.Data.Entities.User.Connections;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +12,29 @@ namespace MiladDarabzadeh.Data.Entities.EnglishExam
 {
     public class Exam
     {
+        [Key]
         public int ExamId { get; set; }
 
+        [Display(Name = "نام تست")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(20, ErrorMessage = "{0} نمیتواند بیشتر از {1} کارکتر باشد")]
+        [MinLength(4, ErrorMessage = "{0} نمیتواند کمتر از {1} کارکتر باشد")]
+        [Column(TypeName = "nvarchar(20)")]
         public string ExamName { get; set; }
-
-    
         
+        public int CountOfQuestions { get; set; }
+
+        [Column(TypeName = "smallint")]
+        public int? Time { get; set; }
+
+        
+        public int CycleId { get; set; }
+
+        [ForeignKey("CycleId")]
+        public CourseCycle CourseCycle { get; set; }
+
+        public List<UserScoreExam> UserScoreExams { get; set; }
+
+        //Question List
     }
 }
---
+++ MiladDarabzadeh.Data/Entities/User/Connections/UserScoreExam.cs
@@ -0,0 +1,35 @@
+﻿using MiladDarabzadeh.Data.Entities.EnglishExam;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiladDarabzadeh.Data.Entities.User.Connections
+{
+    public class UserScoreExam
+    {
+        [Key]
+        public int USEId { get; set; }
+
+        ///||\\\
+       /// ._.\\\
+        [Column(TypeName = "TINYINT")]
+        public int Score { get; set; }
+        public DateTime ExamDate { get; set; }
+
+        public int UserId { get; set; }
+
+        public int ExamId { get; set; }
+
+        [ForeignKey("ExamId")]
+        public Exam Exam { get; set; }
+
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+
+     
+    }
+}
