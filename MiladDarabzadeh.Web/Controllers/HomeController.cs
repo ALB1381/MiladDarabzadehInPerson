65fe176812728de507a759ce8a0d63b654d16507
ali barzegar <alibarzegar013@gmail.com>
2024-08-21 03:11:04 -0700

    #Web

---
+++ MiladDarabzadeh.Web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MiladDarabzadeh.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
